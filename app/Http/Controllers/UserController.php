47c053296a218adffc3d8d42a8b53fa5ad19ed8d
Marek Mikusek <mmikusek2211@gmail.com>
2020-11-24 19:08:17 +0100

    add heroes list caching

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -24,7 +24,7 @@ class UserController extends Controller
     }
 
     public function update(UpdateUserRequest $request){
-        dd($request->validated());
+        // dd($request->validated());
         // if(!$user = $this->userService->update($request->validated()){
         //     return ReturnData::create(['message' => 'error occured', 'code' => 500]);
         // }
--- app/Services/SwapiService.php
+++ app/Services/SwapiService.php
@@ -6,6 +6,7 @@ use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Log;
 use stdClass;
 use GuzzleHttp\Exception\ClientException;
+use Illuminate\Support\Facades\Cache;
 
 class SwapiService
 {
@@ -31,19 +32,34 @@ class SwapiService
         return rand(1, count($this->heroesList));
     }
 
+    public function getResourceList(object $hero): array
+    {
+        $hero = (array)$hero;
+        $ret = [$hero['homeworld']];
+        foreach(['vehicles', 'films', 'starships', 'species'] as $resource){
+            $ret = array_merge($ret, $hero[$resource]);
+        }
+        return $ret;
+    }
+
     public function getHeroesList(): array
     {
+        if ($heroes = Cache::has('heroes_list')){
+            return json_decode(Cache::get('heroes_list'));
+        };
         $heroes = [];
         $pageNo = 1;
         $nextPage = 'http://swapi.dev/api/people/';
         do {
             $page = $this->getApiResponse("{$nextPage}");
             foreach($page->results as $hero){
+                $hero->available_resources = $this->getResourceList($hero);
                 $heroes[] = $hero;
             }
             $pageNo++;
             $nextPage = $page->next;
         } while ($pageNo < 100 && $page && $nextPage);
+        Cache::put('heroes_list', json_encode($heroes), 86400);
         return $heroes;
     }
 
@@ -55,7 +71,6 @@ class SwapiService
                 $endPoint,
                 ['headers' => $this->header]
             );
-            dump(json_decode($apiResponse->getBody()));
             return json_decode($apiResponse->getBody());
         } catch(ClientException $e){
             return new stdClass();
